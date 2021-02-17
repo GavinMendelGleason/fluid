55e01169ed5a61ea61677b0f60b694ebebd22eff
Gavin Mendel-Gleason <gavin@terminusdb.com>
2021-02-17 21:10:04 +0100

    Still changes to be made to determinise interpreter

--- prolog/partial.pl
+++ prolog/partial.pl
@@ -1,29 +1,88 @@
-:- module(partial, [partial_compute_goal]).
+:- module(partial, [
+              goal_program_state/3,
+              partial_compute_goal/3
+          ]).
 
 :- use_module(utilities).
 :- use_module(reify_reflect).
 :- use_module(dict_state).
 
-partial_compute_goal(Goal,Program) :-
-    State = _{
+goal_program_state(Goal,Program,Final_State) :-
+    State = state{
                 program: Program,
-                choices: [],
+                continuation: [],
                 environment: []
             },
-    partial_compute_goal(Goal,State,Final_State),
+    compute_goal(Goal,State,Final_State),
     writeq(Final_State).
 
-partial_compute_goal(P,Q) -->
-    partial_compute_goal(P),
-    partial_compute_goal(Q).
-partial_compute_goal(P;Q) -->
-    push_continuation(goal(Q)),
-    partial_compute_goal(P).
-partial_compute_goal(!) -->
-    pop_continuation.
-partial_compute_goal(functor(Symbol/N,Args)) -->
-    predicate_program(Symbol, N, Program),
-    Program = [clause(Symbol/N, Head, Goal)|Rest],
-    push_continuation(Rest),
+push_continuation(List) -->
+    update(continuation, Old, New),
+    { append(List, Old, New) }.
+
+suffix([],[]).
+suffix([goal(_)|Rest], Rest).
+suffix([continuation(_,_,_)|Rest], Rest).
+
+cut_continuations -->
+    update(continuation, Old, New),
+    { suffix(Old,New) }.
+
+pop_continuations(Continuation -->
+    update(continuation, [, New).
+
+predicate_clauses(Symbol, N, Clauses) -->
+    view(program, Program),
+    { findall(clause(Symbol/N,Args,Goals),
+              member(clause(Symbol/N,Args,Goals), Program),
+              Clauses)
+    }.
+
+
+%! try_compute_goal(G:goal,S0:state,S1:state) is semidet.
+%
+% Try to compute the outcome of a 
+
+try_compute_goal(G) -->
+    true.
+
+compute_goal((P,Q)) -->
+    compute_goal(P),
+    compute_goal(Q).
+compute_goal((P;Q)) -->
+    push_continuation([goal(Q)]),
+    compute_goal(P).
+compute_goal(!) -->
+    cut_continuations.
+compute_goal(functor(Symbol/N,Args)) -->
+    predicate_clauses(Symbol, N, [clause(Symbol/N, Head, Goal)|Rest]),
+    push_continuation(continuation(Args,Rest)),
+    compute_args_head_goal(Args,Head,Goal),
+compute_goal(_) -->
+    pop_continuation(functor(Symbol/N,Args),clause(Symbol/N, Head, Goal)),
+    compute_args_head_goal(Args,Head,Goal).
+
+compute_args_head_goal(Args,Head,Goal) -->
     mapm(unify, Head, Args),
-    partial_compute_goal(Goal).
+    compute_goal(Goal).
+
+:- begin_tests(partial).
+
+test(plus, []) :-
+    Pre_Program = [
+        plus(z,Y,Y),
+        plus(s(X), Y, s(Z)) :-
+            plus(X, Y, Z)],
+
+    maplist(reify_reflect, Pre_Program, Program),
+    fix_vars(Program),
+
+    Pre_Goal = plus(z,s(z), _),
+    reify_reflect_goal(Pre_Goal,Goal),
+    fix_vars(Goal),
+
+    goal_program_state(Goal,Program,State),
+
+    writeq(State).
+
+:- end_tests(partial).
--- prolog/reify_reflect.pl
+++ prolog/reify_reflect.pl
@@ -4,11 +4,12 @@
               free/2,
               bound/2,
               fix_vars/1,
-              unfix_vars/4,
-              unify/3
+              unfix_vars//2,
+              unify//2
           ]).
 
 :- use_module(utilities).
+:- use_module(dict_state).
 
 /*
  * reify_reflect(+,-) is det.
@@ -17,19 +18,23 @@
 reify_reflect((:- Body),declaration(Clauses)) :-
     !,
     xfy_list(',', Body, Clauses).
-reify_reflect((Head --> Body),clause(P//N,Args,Clauses)) :-
+reify_reflect((Head --> Body),clause(P//N,RArgs,Clauses)) :-
     Head =.. [P|Args],
     length(Args,N),
+    maplist(reify_reflect_term,Args,RArgs),
     xfy_list(',', Body, Clauses),
     !.
-reify_reflect((Head :- Body),clause(P/N,Args,[Clause|Clauses])) :-
+reify_reflect((Head :- Body),clause(P/N,RArgs,[RClause|RClauses])) :-
     Head =.. [P|Args],
     length(Args,N),
+    maplist(reify_reflect_term,Args,RArgs),
     xfy_list(',', Body, [Clause|Clauses]),
+    maplist(reify_reflect_goal,[Clause|Clauses],[RClause|RClauses]),
     !.
-reify_reflect(Head,clause(P/N,Args,[])) :-
+reify_reflect(Head,clause(P/N,RArgs,[])) :-
     Head =.. [P|Args],
-    length(Args,N).
+    length(Args,N),
+    maplist(reify_reflect_term,Args,RArgs).
 
 % reify_reflect_goal allows us to take a prolog term and give it a
 % unambiguous representation.
@@ -37,7 +42,7 @@ reify_reflect(Head,clause(P/N,Args,[])) :-
  * reify_reflect_goal(+,-) is det.
  * reify_reflect_goal(-,+) is det.
  */
-reify_reflect_term(X, X) :-
+reify_reflect_term(X, var(X)) :-
     var(X),
     !.
 reify_reflect_term(X, integer(X)) :-
@@ -65,13 +70,14 @@ reify_reflect_goal((A,B), (RA,RB)) :-
     !,
     reify_reflect_goal(A, RA),
     reify_reflect_goal(B, RB).
-reify_reflect_goal((A,B), (A,B)) :-
+reify_reflect_goal((A,B), (RA,RB)) :-
     !,
     reify_reflect_goal(A, RA),
     reify_reflect_goal(B, RB).
-reify_reflect_goal(Functor, call(Goal,Args)) :-
-    Functor =.. [call|Args],
-    !.
+reify_reflect_goal(Functor, call(RGoal,Reflect_Args)) :-
+    Functor =.. [call,Goal|Reify_Args],
+    !,
+    reify_reflect_goal(Goal, RGoal),
     maplist(reify_reflect_term,Reify_Args,Reflect_Args).
 reify_reflect_goal(Functor, functor(Symbol/N,Reflect_Args)) :-
     Functor =.. [Symbol|Reify_Args],
@@ -103,7 +109,7 @@ fix_vars(Term) :-
     term_variables(Term, Variables),
     maplist([Var]>>(
                 gensym('V', X),
-                Var = var(X)
+                Var = X
             ), Variables).
 
 % needs to walk the entire syntax.
@@ -111,11 +117,38 @@ unfix_vars(integer(X),integer(X)) --> [].
 unfix_vars(float(X),float(X)) --> [].
 unfix_vars(functor(F,L),float(F,M)) -->
     mapm(unfix_vars, L, M).
+/*
 unfix_vars(lambda(FreeX, BoundX, GoalX),lambda(FreeY, BoundY, GoalY)) -->
     {throw(error(unimplemented))}.
+*/
+
+:- begin_tests(reify_reflect).
+
+test(plus_goal_reify,[]) :-
+    Pre_Goal = plus(z,s(z), _),
+    reify_reflect_goal(Pre_Goal,Goal),
+    Goal = functor(plus/3,[functor(z/0,[]),
+                           functor(s/1,[functor(z/0,[])]),
+                           var(_)]).
+
+test(plus_reify,[]) :-
+
+    Pre_Program = [
+        plus(z,Y,Y),
+        plus(s(X), Y, s(Z)) :-
+            plus(X, Y, Z)],
+
+    maplist(reify_reflect, Pre_Program, Program),
+    fix_vars(Program),
+    Program = [clause(plus/3,[functor(z/0,[]),var(A),var(A)],[]),
+               clause(plus/3,[functor(s/1,[var(E)]),var(F),functor(s/1,[var(G)])],
+                      [functor(plus/3,[var(E),var(F),var(G)])])].
+
+
+:- end_tests(reify_reflect).
 
 extend(X, R) -->
-    update(enviornment, Env, [X-R|Env]).
+    update(environment, Env, [X-R|Env]).
 
 lookup(Y, R) -->
     view(environment, Env),
@@ -128,9 +161,40 @@ unify(var(X), var(Y)) -->
         ;   extend(X,R))
     ;   extend(X,var(Y))
     ).
-unify(functor(P/N, L), var(X)) -->
-    unify(var(X), functor(P/N, L)).
 unify(var(X), functor(P/N, L)) -->
     extend(X,functor(P/N, L)).
+unify(functor(P/N, L), var(X)) -->
+    unify(var(X), functor(P/N, L)).
 unify(functor(P/N, L1), functor(P/N, L2)) -->
     mapm(unify, L1, L2).
+
+:- begin_tests(unification).
+
+test(var_var, []) :-
+    unify(var(x),var(y), state{ environment : []}, State),
+    !,
+    lookup(x,var(y),State,_).
+
+test(var_atom, []) :-
+    unify(var(x),functor(z/0,[]), state{ environment : []}, State),
+    !,
+    lookup(x,functor(z/0,[]),State,_).
+
+test(atom_var, []) :-
+    unify(functor(z/0,[]), var(x), state{ environment : []}, State),
+    !,
+    lookup(x,functor(z/0,[]),State,_).
+
+test(atom_atom, []) :-
+    unify(functor(z/0,[]), functor(z/0,[]), state{ environment : []}, _State),
+    !.
+
+test(functor_functor, []) :-
+    unify(functor(f/0,[var(x),functor(a/0,[])]),
+          functor(f/0,[functor(b/0,[]),var(y)]),
+          state{ environment : []}, State),
+    !,
+    lookup(x,functor(b/0,[]),State,_),
+    lookup(y,functor(a/0,[]),State,_).
+
+:- end_tests(unification).
